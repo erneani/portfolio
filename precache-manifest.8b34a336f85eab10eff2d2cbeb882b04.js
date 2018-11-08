77050505c3891b06e7c525cdd38da50553d159a0
vinicius <vinicius@tootz.com.br>
2018-11-07 23:36:13 -0300

    Updates

---
+++ precache-manifest.8b34a336f85eab10eff2d2cbeb882b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f888b0b5a32501e201c3",
+    "url": "/portfolio/static/js/main.f888b0b5.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/portfolio/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "f888b0b5a32501e201c3",
+    "url": "/portfolio/static/css/main.9491e995.chunk.css"
+  },
+  {
+    "revision": "20e07b9980a91e92601c47c614f5b20b",
+    "url": "/portfolio/index.html"
+  }
+];
